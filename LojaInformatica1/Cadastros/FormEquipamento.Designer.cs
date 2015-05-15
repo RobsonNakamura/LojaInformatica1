7804e38f950a469618b3d46f6f18de6f32f7335b
RobsonNakamura <nakamura.sistemas@gmail.com>
2015-05-15 11:59:27 -0300

    fromEquipamento

---
+++ LojaInformatica1/Cadastros/FormEquipamento.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace LojaInformatica1.Cadastros
+{
+    partial class FormEquipamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEquipamento = new System.Windows.Forms.Label();
+            this.lblCodEquipamento = new System.Windows.Forms.Label();
+            this.lblTipoEquipamento = new System.Windows.Forms.Label();
+            this.lblModelo = new System.Windows.Forms.Label();
+            this.lblNumSerie = new System.Windows.Forms.Label();
+            this.lblFabricante = new System.Windows.Forms.Label();
+            this.lblDataEntradaEquipamento = new System.Windows.Forms.Label();
+            this.lblObservaçãoEquipamento = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.cbxTipoequipamento = new System.Windows.Forms.ComboBox();
+            this.btnSalvarEquipamento = new System.Windows.Forms.Button();
+            this.btnCancelarEquipamento = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEquipamento
+            // 
+            this.lblEquipamento.AutoSize = true;
+            this.lblEquipamento.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEquipamento.Location = new System.Drawing.Point(35, 35);
+            this.lblEquipamento.Name = "lblEquipamento";
+            this.lblEquipamento.Size = new System.Drawing.Size(231, 22);
+            this.lblEquipamento.TabIndex = 13;
+            this.lblEquipamento.Text = "Cadastro de Equipamentos";
+            // 
+            // lblCodEquipamento
+            // 
+            this.lblCodEquipamento.AutoSize = true;
+            this.lblCodEquipamento.Location = new System.Drawing.Point(36, 104);
+            this.lblCodEquipamento.Name = "lblCodEquipamento";
+            this.lblCodEquipamento.Size = new System.Drawing.Size(108, 13);
+            this.lblCodEquipamento.TabIndex = 14;
+            this.lblCodEquipamento.Text = "Código Equipamento:";
+            // 
+            // lblTipoEquipamento
+            // 
+            this.lblTipoEquipamento.AutoSize = true;
+            this.lblTipoEquipamento.Location = new System.Drawing.Point(36, 149);
+            this.lblTipoEquipamento.Name = "lblTipoEquipamento";
+            this.lblTipoEquipamento.Size = new System.Drawing.Size(170, 13);
+            this.lblTipoEquipamento.TabIndex = 14;
+            this.lblTipoEquipamento.Text = "Selecione o Tipo do Equipamento:";
+            // 
+            // lblModelo
+            // 
+            this.lblModelo.AutoSize = true;
+            this.lblModelo.Location = new System.Drawing.Point(36, 194);
+            this.lblModelo.Name = "lblModelo";
+            this.lblModelo.Size = new System.Drawing.Size(45, 13);
+            this.lblModelo.TabIndex = 14;
+            this.lblModelo.Text = "Modelo:";
+            // 
+            // lblNumSerie
+            // 
+            this.lblNumSerie.AutoSize = true;
+            this.lblNumSerie.Location = new System.Drawing.Point(36, 239);
+            this.lblNumSerie.Name = "lblNumSerie";
+            this.lblNumSerie.Size = new System.Drawing.Size(89, 13);
+            this.lblNumSerie.TabIndex = 14;
+            this.lblNumSerie.Text = "Número de Série:";
+            // 
+            // lblFabricante
+            // 
+            this.lblFabricante.AutoSize = true;
+            this.lblFabricante.Location = new System.Drawing.Point(36, 284);
+            this.lblFabricante.Name = "lblFabricante";
+            this.lblFabricante.Size = new System.Drawing.Size(60, 13);
+            this.lblFabricante.TabIndex = 14;
+            this.lblFabricante.Text = "Fabricante:";
+            // 
+            // lblDataEntradaEquipamento
+            // 
+            this.lblDataEntradaEquipamento.AutoSize = true;
+            this.lblDataEntradaEquipamento.Location = new System.Drawing.Point(36, 329);
+            this.lblDataEntradaEquipamento.Name = "lblDataEntradaEquipamento";
+            this.lblDataEntradaEquipamento.Size = new System.Drawing.Size(88, 13);
+            this.lblDataEntradaEquipamento.TabIndex = 14;
+            this.lblDataEntradaEquipamento.Text = "Data de Entrada:";
+            // 
+            // lblObservaçãoEquipamento
+            // 
+            this.lblObservaçãoEquipamento.AutoSize = true;
+            this.lblObservaçãoEquipamento.Location = new System.Drawing.Point(36, 374);
+            this.lblObservaçãoEquipamento.Name = "lblObservaçãoEquipamento";
+            this.lblObservaçãoEquipamento.Size = new System.Drawing.Size(68, 13);
+            this.lblObservaçãoEquipamento.TabIndex = 14;
+            this.lblObservaçãoEquipamento.Text = "Observação:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(223, 97);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 20);
+            this.textBox1.TabIndex = 15;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(223, 187);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(190, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(223, 232);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(190, 20);
+            this.textBox4.TabIndex = 15;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(223, 277);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(190, 20);
+            this.textBox5.TabIndex = 15;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(110, 374);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(303, 167);
+            this.textBox7.TabIndex = 15;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(223, 322);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(190, 20);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // cbxTipoequipamento
+            // 
+            this.cbxTipoequipamento.FormattingEnabled = true;
+            this.cbxTipoequipamento.Items.AddRange(new object[] {
+            "MicroComputador",
+            "Monitor",
+            "Notebook",
+            "Netbook",
+            "Impressora Jato de Tinta",
+            "Impressora Laser"});
+            this.cbxTipoequipamento.Location = new System.Drawing.Point(223, 140);
+            this.cbxTipoequipamento.Name = "cbxTipoequipamento";
+            this.cbxTipoequipamento.Size = new System.Drawing.Size(190, 21);
+            this.cbxTipoequipamento.TabIndex = 17;
+            this.cbxTipoequipamento.Text = "Selecione um Equipamento...";
+            // 
+            // btnSalvarEquipamento
+            // 
+            this.btnSalvarEquipamento.Location = new System.Drawing.Point(110, 564);
+            this.btnSalvarEquipamento.Name = "btnSalvarEquipamento";
+            this.btnSalvarEquipamento.Size = new System.Drawing.Size(122, 35);
+            this.btnSalvarEquipamento.TabIndex = 18;
+            this.btnSalvarEquipamento.Text = "Salvar";
+            this.btnSalvarEquipamento.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelarEquipamento
+            // 
+            this.btnCancelarEquipamento.Location = new System.Drawing.Point(291, 564);
+            this.btnCancelarEquipamento.Name = "btnCancelarEquipamento";
+            this.btnCancelarEquipamento.Size = new System.Drawing.Size(122, 35);
+            this.btnCancelarEquipamento.TabIndex = 18;
+            this.btnCancelarEquipamento.Text = "Cancelar";
+            this.btnCancelarEquipamento.UseVisualStyleBackColor = true;
+            // 
+            // FormEquipamento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 622);
+            this.Controls.Add(this.btnCancelarEquipamento);
+            this.Controls.Add(this.btnSalvarEquipamento);
+            this.Controls.Add(this.cbxTipoequipamento);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblObservaçãoEquipamento);
+            this.Controls.Add(this.lblDataEntradaEquipamento);
+            this.Controls.Add(this.lblFabricante);
+            this.Controls.Add(this.lblNumSerie);
+            this.Controls.Add(this.lblModelo);
+            this.Controls.Add(this.lblTipoEquipamento);
+            this.Controls.Add(this.lblCodEquipamento);
+            this.Controls.Add(this.lblEquipamento);
+            this.Name = "FormEquipamento";
+            this.Text = "FormEquipamento";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEquipamento;
+        private System.Windows.Forms.Label lblCodEquipamento;
+        private System.Windows.Forms.Label lblTipoEquipamento;
+        private System.Windows.Forms.Label lblModelo;
+        private System.Windows.Forms.Label lblNumSerie;
+        private System.Windows.Forms.Label lblFabricante;
+        private System.Windows.Forms.Label lblDataEntradaEquipamento;
+        private System.Windows.Forms.Label lblObservaçãoEquipamento;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox cbxTipoequipamento;
+        private System.Windows.Forms.Button btnSalvarEquipamento;
+        private System.Windows.Forms.Button btnCancelarEquipamento;
+
+    }
+}
